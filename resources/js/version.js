67aae73272431327986173d134271217015da26a
Crimtea <Metlis@outlook.com>
2024-01-14 02:07:22 +0800

    update

--- resources/js/version.js
+++ resources/js/version.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 // 向页脚插入版本信息
-fetch('./resources/public/version.txt') // 获取文件
+fetch('/WordTrain/resources/public/version.txt') // 获取文件
 .then(response => response.text())
 .then(data => {
 	document.getElementById('content').innerText = data;
